b283e4e91080d63d8650c3a01063e4f9fac1a62d
Abuobaed <abuobaedashik@gmail.com>
2024-09-25 02:08:34 +0600

    commit add

--- js/Fiture.js
+++ js/Fiture.js
@@ -15,12 +15,14 @@ document.getElementById('donation-btn-1').addEventListener('click',function (eve
     if (noakhliDonationInput>0 && typeof noakhliDonationInput === 'number') {
         document.getElementById('total-balance-noakhali').innerText =noakhliFoundAdd;
         document.getElementById('total-balance').innerText =minusFromTotal;
-        // console.log(noakhliDonationInput)
+        // modal
+        document.getElementById('my-modal').showModal();
         // history added 1
         const record1 =document.createElement('p');
         record1.innerText=`${noakhliDonationInput} Taka is Donated for Flood Relief in Noakhali,Bangladesh`;
         const historyGet = document.getElementById('history-addrd').appendChild(record1);
         historyGet.classList.add('history-card')
+        document.getElementById('my-modal').showModal();
     }
     else{
        alert('Invalid Amount!');
@@ -30,6 +32,7 @@ document.getElementById('donation-btn-1').addEventListener('click',function (eve
 document.getElementById('donation-btn-2').addEventListener('click',function (event) {
     event.preventDefault();
     // total balance
+    
     const totalBalance = document.getElementById('total-balance').innerText;
     const totalBalanceNum =parseFloat(totalBalance);
     // input and money add
@@ -41,12 +44,14 @@ document.getElementById('donation-btn-2').addEventListener('click',function (eve
     if (feniDonationInput>0 && typeof feniDonationInput === 'number') {
         document.getElementById('total-balance-feni').innerText =feniFoundAdd;
         document.getElementById('total-balance').innerText =minusFromTotal;
+        // modal
+        document.getElementById('my-modal').showModal();
         // history added 2
         const time =date();
         const record2 =document.createElement('p');
         record2.innerText=`${feniDonationInput} Taka is Donated for Flood Relief in Feni,Bangladesh ${time}`;
         const historyGet2 = document.getElementById('history-addrd').appendChild(record2);
-        historyGet2.classList.add('history-card');
+        historyGet2.classList.add('history-card');  
     }
     else{
        alert('Invalid Amount!');
@@ -67,13 +72,14 @@ document.getElementById('donation-btn-3').addEventListener('click',function (eve
      if (quotaDonationInput>0 && typeof quotaDonationInput === 'number') {
         document.getElementById('total-balance-quota').innerText =quotaFoundAdd;
         document.getElementById('total-balance').innerText =minusFromTotal;
+        // modal
+        document.getElementById('my-modal').showModal();
         // history added 3
         const record3 =document.createElement('p');
         record3.innerText=`${quotaDonationInput} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh`;
          const historyGet3 = document.getElementById('history-addrd').appendChild(record3);
         historyGet3.classList.add('history-card')
-        const time = date()
-         console.log(time);
+        
     }
     else{
        alert('Invalid Amount!');

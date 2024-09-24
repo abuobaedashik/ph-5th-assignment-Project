4de84504bf0d3cfcaa49f46e36335436bc43af56
Abuobaed <abuobaedashik@gmail.com>
2024-09-24 13:21:17 +0600

    Commit And revice

--- js/Fiture.js
+++ js/Fiture.js
@@ -12,7 +12,7 @@ document.getElementById('donation-btn-1').addEventListener('click',function (eve
     const minusFromTotal =totalBalanceNum-noakhliDonationInput;
     //  show add and total money 
     
-    if (noakhliDonationInput>=0 && typeof noakhliDonationInput === 'number') {
+    if (noakhliDonationInput>0 && typeof noakhliDonationInput === 'number') {
         document.getElementById('total-balance-noakhali').innerText =noakhliFoundAdd;
         document.getElementById('total-balance').innerText =minusFromTotal;
         // console.log(noakhliDonationInput)
@@ -38,12 +38,13 @@ document.getElementById('donation-btn-2').addEventListener('click',function (eve
     const feniFoundAdd =feniDonationInput+feniDonationMoney;
     const minusFromTotal =totalBalanceNum-feniDonationInput;
     //  show add and total money 
-    if (feniDonationInput>=0 && typeof feniDonationInput === 'number') {
+    if (feniDonationInput>0 && typeof feniDonationInput === 'number') {
         document.getElementById('total-balance-feni').innerText =feniFoundAdd;
         document.getElementById('total-balance').innerText =minusFromTotal;
         // history added 2
+        const time =date();
         const record2 =document.createElement('p');
-        record2.innerText=`${feniDonationInput} Taka is Donated for Flood Relief in Feni,Bangladesh`;
+        record2.innerText=`${feniDonationInput} Taka is Donated for Flood Relief in Feni,Bangladesh ${time}`;
         const historyGet2 = document.getElementById('history-addrd').appendChild(record2);
         historyGet2.classList.add('history-card');
     }
@@ -63,7 +64,7 @@ document.getElementById('donation-btn-3').addEventListener('click',function (eve
     const quotaFoundAdd =quotaDonationInput+quotaDonationMoney;
     const minusFromTotal =totalBalanceNum-quotaDonationInput;
      //  show add and total money 
-     if (quotaDonationInput>=0 && typeof quotaDonationInput === 'number') {
+     if (quotaDonationInput>0 && typeof quotaDonationInput === 'number') {
         document.getElementById('total-balance-quota').innerText =quotaFoundAdd;
         document.getElementById('total-balance').innerText =minusFromTotal;
         // history added 3
@@ -71,10 +72,22 @@ document.getElementById('donation-btn-3').addEventListener('click',function (eve
         record3.innerText=`${quotaDonationInput} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh`;
          const historyGet3 = document.getElementById('history-addrd').appendChild(record3);
         historyGet3.classList.add('history-card')
+        const time = date()
+         console.log(time);
     }
     else{
        alert('Invalid Amount!');
     } 
 });
+
+document.getElementById('blog-btn').addEventListener('click',function () {
+    window.location.href='./blog.html';
+    // window.location.href='./blog.html';
+})
+document.getElementById('back-home').addEventListener('click',function () {
+    window.location.href='./index.html';
+    // window.location.href='./index.html';
+    console.log('get');
+})
 
 
--- js/utilites.js
+++ js/utilites.js
@@ -8,6 +8,14 @@ function commonBalanceIdGet(id) {
   const donationNum =parseFloat(donation);
     return donationNum;
 }
+// date funtion
+function date() {
+  var date = new Date();
+  var str = date.toString();
+  return str ;
+}
+
+
 
 
 
